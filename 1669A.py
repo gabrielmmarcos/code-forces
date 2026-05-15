76b0f658fbdee24000ce0e4b0aee66630af96db8
GabrielMMMMM <gabrielmmarcos7@gmail.com>
2026-05-14 21:42:50 -0300

    exercicio 1669a

---
+++ 1669A.py
@@ -0,0 +1,12 @@
+ratings = int(input(''))
+
+for _ in range(ratings):
+    rating = int(input(''))
+    if rating <= 1399:
+        print('Division 4')
+    elif rating <= 1599:
+        print('Division 3')
+    elif rating <= 1899:
+        print('Division 2')
+    else:
+        print('Division 1')
